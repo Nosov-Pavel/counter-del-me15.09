8ad29d705b107660a8166b69e5b0d9d458cf30c4
Nosov-Pavel <nopaev@gmail.com>
2020-10-20 23:44:42 -0400

    deleteById

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, {useState} from 'react';
 
 const initialCounters = [
-    { id: 1, title: 'Green', count: 5},
-    { id: 2, title: 'Pink', count: 50}
-    ];
+    {id: 1, title: 'Green', count: 5},
+    {id: 2, title: 'Pink', count: 50},
+    {id: 3, title: 'Orange', count: 7},
+];
 
 function App() {
-    const [counters, setCounters] = useState(initialCounters)
+    const [counters, setCounters] = useState(initialCounters);
 
-  return (
-    <div>
-        Counters
-
-        <ul>
-            {counters.map(el => <li key={el.id}>{el.title} {el.count}</li>)}
-
-        </ul>
+    const deleteById = (id) => {
+        console.log(id);
+    };
 
 
+    return (
+        <div>
+            Counters
+
+            <ul>
+                {counters.map(el => <li key={el.id}>{el.title}
+                    <button>-</button>
+                    {el.count}
+                    <button>+</button>
+                    <button onClick={() => deleteById(el.id)}>Delete</button>
+                    <button>Up</button>
+                    <button>Down</button>
+                </li>)}
+
+            </ul>
 
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
