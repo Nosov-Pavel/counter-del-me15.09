1b37424e52af5b9b284d9427251683b7ee52cdfd
Nosov-Pavel <nopaev@gmail.com>
2020-12-14 13:34:47 -0500

    button+-

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ function App(){
     const initialCounters = [0,0,0];
     const [counters,setCounters] = useState(initialCounters);
 
+    const addCounter = {
+        const
+    }
+
 
     return(
       <div className="App">
+          <button>Add</button>
           {counters.map(el => <li>
-              <button></button>
-              {el}</li>)}
+              <button>+</button>
+              {el}
+              <button>-</button>
+          </li>)}
       </div>
 
     );
