cc802abc19c0357d7059fbcb4652bdfe5ac0b4bf
Nosov-Pavel <nopaev@gmail.com>
2020-12-14 13:22:55 -0500

    cl

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {useState} from "react";
+import './App.css';
 
 function App(){
-    const initialCounters = [0,0,0];
+    const initionCounters = [0,0,0];
+    const [counters,setCounters] = useState(initionCounters);
 
-    const [counters, setCounters] = useState(initialCounters)
+
     return(
-        <div className="App">
-            {counters.map(el => <li> {el} </li>)}
+      <div className="App">
+          {counters.map(el => <li>{el}</li>)}
+      </div>
 
-        </div>
     );
 }
 
