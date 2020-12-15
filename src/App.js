b88b5077f50ac1c08caf2514b8a766416b8904b6
Nosov-Pavel <nopaev@gmail.com>
2020-12-15 13:57:43 -0500

    CounDuble

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import "./App.css"
 function App(){
     const initialCounters = [0,0,0];
     const [counters, setCounters] = useState(initialCounters);
-
+const intCounters = [0,0,0];
+const [counters1, setCounters1]= useState(intCounters)
     return(
         <div className='App'>
             {counters}
+            <br/>
+            {counters1}
         </div>
     );
 }
