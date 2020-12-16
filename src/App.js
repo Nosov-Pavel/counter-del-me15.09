d40a09f44aeb578dd15e24c1996168137f507d30
Nosov-Pavel <nopaev@gmail.com>
2020-12-16 11:36:22 -0500

    CounDuble3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {useState} from "react";
 import "./App.css"
 
-function App(){
-    const initialCounters = [0,0,0];
+function App() {
+    const initialCounters = [0, 0, 0];
     const [counters, setCounters] = useState(initialCounters);
-const intCounters = [0,0,0];
-const [counters1, setCounters1]= useState(intCounters)
-    return(
+
+    const intCounters = [0, 0, 0];
+    const [counters1, setCounters1] = useState(intCounters);
+
+    const moreCounters =[0,0,0];
+    const [counters3, setCounters3] = useState(moreCounters);
+
+    return (
         <div className='App'>
             {counters}
             <br/>
             {counters1}
+            <br/>
+            {counters3}
         </div>
     );
 }
